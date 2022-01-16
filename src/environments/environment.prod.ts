2d61819fec2ededf65fcf14067321c7b78cbdd34
inpercima <inpercima@gmail.com>
2022-01-16 15:22:37 +0100

    Update gitignore and publish environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  api: 'https://www.mit-dem-rad-zur-arbeit.de/api/teamranking.php?trid=98',
+  appname: 'MdRzA Ranking',
+  defaultRoute: 'dashboard',
+  production: false,
+  theme: 'indigo-pink',
+};

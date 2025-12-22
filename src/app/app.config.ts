df5f57b3cbb2d4b9e3664522e62ae1fa0476282e
Marcel Hendrich <inpercima@gmail.com>
2025-12-22 21:21:06 +0100

    refactor(build): make app zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient()],
+  providers: [provideBrowserGlobalErrorListeners(), provideHttpClient()],
 };
